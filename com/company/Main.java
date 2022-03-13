f3affda5af177275938407519f70d4f75eb0df56
katrich1996 <sergey.katrich96@mail.ru>
2022-03-13 20:23:44 +0300

    Test

---
+++ com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+int age = 18;
+if (age == 18) {
+    System.out.printf("С окончанием школы");
+} else if (age == 21) {
+    System.out.println("Теперь тебе можно пить алкоголь");
+} else if (age == 7) {
+    System.out.println("Пока сказать нечего");
+}
+    }
+}
+
+
+
